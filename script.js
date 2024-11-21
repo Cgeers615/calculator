96797b7474a6bbdd1c5733bea7ce92b90c86d03f
Cgeers615 <25189222+Cgeers615@users.noreply.github.com>
2024-11-21 10:18:38 -0500

    create math functions

--- script.js
+++ script.js
@@ -8,14 +8,24 @@ Write 4 functions that represent the basic calcuator functions of add, subtract,
 */
 
 //add
+function addNum(a, b) {
+    return a + b;
+};
 
 //subtract
+function subNum (a, b) {
+    return a - b;
+}
 
 //multiply
-
+function multiplyNum(a, b) {
+    return a * b;
+}
 
 //divide
-
+function divideNum(a, b) {
+    return a / b;
+}
 /* Step 2
 
 */ 
