375f2dfc11e16731fa820bc7cedd6eb8ecbfe922
Cgeers615 <25189222+Cgeers615@users.noreply.github.com>
2024-11-21 09:46:30 -0500

    create file and early documenting
    Documentation in this upload is for step 1 and the creation of step 2

---
+++ script.js
@@ -0,0 +1,21 @@
+/* Web Browser Calculator */
+
+
+
+/* Step 1) 
+Write 4 functions that represent the basic calcuator functions of add, subtract, multiple and divide
+
+*/
+
+//add
+
+//subtract
+
+//multiply
+
+
+//divide
+
+/* Step 2
+
+*/ 
