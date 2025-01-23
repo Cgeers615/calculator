48ee1f1c5f978bccea606c7ee06729d1e9ba5d63
Courtney Scaccia <25189222+Cgeers615@users.noreply.github.com>
2025-01-23 15:33:38 -0500

    final old script

--- old verison/script.js
+++ old verison/script.js
@@ -194,18 +194,3 @@ clearBtn.addEventListener('click',clear);
 
 
 
-/* Add Keyboard support - plan: **LOOK INTO DATA KEYS **
-  1) make equationTxt into a text input 
-  2) listen for keys 0-9, +, -, *, /, ., enter to be PRESSED down 
-        when keys 0-9 are pressed down, reflect the value of said key in the equationTxt
-        when +, -, *, / key is pressed down, add that to the equationTxt 
-        when enter is pressed run the calculation function 
-  
-*/
-
-
-/*Extra Credit:
-    - Users can get floating point numbers if they do the math required to get one, but they can’t type them in yet. Add a . button and let users input decimals! Make sure you don’t let them type more than one though, like: 12.3.56.5. Disable the . button if there’s already a decimal separator in the display. --unsure
-    - Add keyboard support! <-- doable
-
-*/ 
